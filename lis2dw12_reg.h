24f0ec8dd87834ec9a2cc5ee008599a1bc02a8eb
Armando Visconti <armando.visconti@st.com>
2022-12-20 12:02:20 +0100

    Add __weak directive to read/write registers routines
    Added __weak directive to registers read/write routines,
    to allow the final application to overwrite them with a
    custom implementation.
    
    The driver keeps offering a default implementation based
    on function pointers to read/write routines for backward
    compatibility.
    
    Signed-off-by: Armando Visconti <armando.visconti@st.com>

--- lis2dw12_reg.h
+++ lis2dw12_reg.h
@@ -656,6 +656,19 @@ typedef union
   *
   */
 
+#ifndef __weak
+#define __weak __attribute__((weak))
+#endif /* __weak */
+
+/*
+ * These are the basic platform dependent I/O routines to read
+ * and write device registers connected on a standard bus.
+ * The driver keeps offering a default implementation based on function
+ * pointers to read/write routines for backward compatibility.
+ * The __weak directive allows the final application to overwrite
+ * them with a custom implementation.
+ */
+
 int32_t lis2dw12_read_reg(stmdev_ctx_t *ctx, uint8_t reg,
                           uint8_t *data,
                           uint16_t len);
